804247ba6f7c44ce1c08faf0c28115328c0600c1
eden <edenwzg@outlook.com>
2017-10-22 11:06:59 +0800

    text

--- notepad.cpp
+++ notepad.cpp
@@ -4,7 +4,10 @@ git
     git config --global user.name "Your Name"
     ssh-keygen -t rsa -C "youremail@example.com"
     git clone https://github.com/vnpy/vnpy.git
-    git add
+    'add'
+        git add -A  // 提交所有变化
+        git add -u  // 提交被修改(modified)和被删除(deleted)文件，不包括新文件(new)
+        git add .   // 提交新文件(new)和被修改(modified)文件，不包括被删除(deleted)文件
     git status
     git commit -m "Describe Text"
 PowerShell
